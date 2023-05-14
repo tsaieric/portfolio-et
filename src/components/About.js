eb3522514795de684b8bcd5c8172c20787d2e1a2
tsaieric <erictsai.dev@gmail.com>
2023-05-14 04:05:13 -0700

    Fix padding for about me/project sections

--- src/components/About.js
+++ src/components/About.js
@@ -149,7 +149,7 @@ const AboutWrapper = styled.div`
   margin-right: auto;
   margin-left: auto;
   //set min-height to fix overlapping on width < 400px
-  min-height: 800px;
+  min-height: 900px;
   /* padding: 0rem calc((100vw-1300px) / 2); */
   padding: 0 12px;
   justify-content: center;
