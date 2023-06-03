872b0be26cb36e7584bf0b3cb9033d2132a01d1d
tsaieric <erictsai.dev@gmail.com>
2023-06-03 15:00:53 -0700

    set redirect links for career

--- src/data/careerElements.js
+++ src/data/careerElements.js
@@ -4,8 +4,13 @@ let careerElements = [
     title: "Google Stadia/Youtube",
     subtitle: "Software Engineer (Java, C++, Dart)",
     list: [
+<<<<<<< HEAD
       "Reduced re-upload spam impacting 1M+ YouTube Shorts by developing a pipeline to index video risk signals identified by TensorFlow and ML models.",
       "Designed & implemented a Stadia recommendation system to auto-suggest free games based on trending games, user preferences, user history, and region.",
+=======
+      "Reduced Youtube Shorts spam-abuse by creating a pipeline to index risk scores identified by ML/TensorFlow models.",
+      "Developed Stadia features to improve discoverability like adding emoji reactions.",
+>>>>>>> 310d68d (set redirect links for career)
     ],
     date: "August 2020 - March 2023",
     icon: "work",
@@ -16,12 +21,17 @@ let careerElements = [
     title: "Amazon",
     subtitle: "Software Development Engineer (Java, C++)",
     list: [
+<<<<<<< HEAD
       "Added telemetry features for the Alexa Auto SDK which integrates Alexa into vehicles.",
       "Implemented Java & C++ modules that allow OEM partners to capture latency & vehicle metadata from the Alexa Auto SDK and upload to Amazon CloudWatch, Amazon EMR, etc.",
+=======
+      "Created features for the Alexa Auto SDK to integrate Alexa into vehicle head units.",
+      "Implemented Java, C++ modules that allowed OEM partners to capture latency + vehicle metadata from the Alexa Auto SDK and upload to Amazon Cloud.",
+>>>>>>> 310d68d (set redirect links for career)
     ],
     date: "April 2018 - January 2020",
     icon: "work",
-    redirectLink: "https://www.youtube.com/hashtag/shorts",
+    redirectLink: "https://developer.amazon.com/en-US/alexa/alexa-auto/sdk",
   },
   {
     id: 3,
@@ -33,7 +43,7 @@ let careerElements = [
     ],
     date: "September 2016 - March 2018",
     icon: "work",
-    redirectLink: "https://www.youtube.com/hashtag/shorts",
+    redirectLink: "https://www.ge.com/digital/",
   },
   {
     id: 4,
@@ -45,7 +55,7 @@ let careerElements = [
     ],
     date: "May 2015 - August 2015",
     icon: "work",
-    redirectLink: "https://www.youtube.com/hashtag/shorts",
+    redirectLink: "https://www.simcoachgames.com/",
   },
   {
     id: 5,
@@ -54,7 +64,7 @@ let careerElements = [
     description: "Master of Entertainment Technology",
     date: "2014 - 2016",
     icon: "school",
-    redirectLink: "https://www.youtube.com/hashtag/shorts",
+    redirectLink: "https://www.etc.cmu.edu/",
   },
   {
     id: 6,
@@ -63,7 +73,7 @@ let careerElements = [
     description: "B.S. Computer Science",
     date: "2010 - 2014",
     icon: "school",
-    redirectLink: "https://www.youtube.com/hashtag/shorts",
+    redirectLink: "https://engineering.virginia.edu/departments/computer-science",
   },
 ];
 
